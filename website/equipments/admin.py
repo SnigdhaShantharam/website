3c107a936f49015ac96238f21a0646e3a02edbfd
SnigdhaShantharam <57173568+SnigdhaShantharam@users.noreply.github.com>
2020-03-22 02:02:09 +0530

    [Snigdha]Add. admin customization(version 1)

--- website/equipments/admin.py
+++ website/equipments/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Camera, Lens, Accessories, Equipments
+
+admin.site.site_header = "Padmashree Associates"
+admin.site.site_title = "Padmashree Associates"
+admin.site.index_title = 'Administrator Dashboard'
+
+class EquipmentsInline(admin.StackedInline):
+    model = Equipments
+    fields = ['image']
+    extra = 1
+
+class CameraAdmin(admin.ModelAdmin):
+    inlines = [EquipmentsInline, ]
+    list_display = ['company', 'model']
+    fields = ['company', 'model', 'description', 'inventory', 'ratings']
+
+class LensAdmin(admin.ModelAdmin):
+    inlines = [EquipmentsInline, ]
+    list_display = ['company', 'model']
+    fields = ['company', 'model', 'description', 'inventory', 'ratings']
+
+class AccessoriesAdmin(admin.ModelAdmin):
+    inlines = [EquipmentsInline, ]
+    list_display = ['company', 'model']
+    fields = ['company', 'model', 'description', 'inventory', 'ratings']
+
+admin.site.register(Camera, CameraAdmin)
+admin.site.register(Lens, LensAdmin)
+admin.site.register(Accessories, AccessoriesAdmin)
