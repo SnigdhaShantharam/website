dcb631e68b0b7a5d38bc44c55169fcfe2e10ba1d
SnigdhaShantharam <snigdhashantharam@gmail.com>
2020-03-22 01:35:32 +0530

    [Snigdha]Refactor. admin customization

--- website/equipments/models.py
+++ website/equipments/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+from PIL import Image
+
+ratings = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+)
 class Camera(models.Model):
     company     = models.CharField(max_length=50)
     model       = models.CharField(max_length=50)
     description = models.TextField()
     inventory   = models.IntegerField()
-    ratings     = models.models.FloatField(min=1, max=5)
+    ratings     = models.IntegerField(choices=ratings)
 
     def __str__(self):
         return '{} {}'.format(self.company, self.model)
@@ -19,8 +28,8 @@ class Lens(models.Model):
     model       = models.CharField(max_length=50)
     description = models.TextField()
     inventory   = models.IntegerField()
-    ratings     = models.models.FloatField(min=1, max=5)
-
+    ratings     = models.IntegerField(choices=ratings)
+    
     def __str__(self):
         return '{} {}'.format(self.company, self.model)
 
@@ -34,11 +43,21 @@ class Accessories(models.Model):
     name        = models.CharField(max_length=150)
     description = models.TextField()
     inventory   = models.IntegerField()
-    ratings     = models.models.FloatField(min=1, max=5)
-
+    ratings     = models.IntegerField(choices=ratings)
+    
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = "Accessories"
         verbose_name_plural = "Accessories"
+
+class Equipments(models.Model):
+    camera_key = models.ForeignKey(Camera, on_delete=models.CASCADE, null=True, blank=True)
+    lens_key = models.ForeignKey(Lens, on_delete=models.CASCADE, null=True, blank=True)
+    accessories_key = models.ForeignKey(Accessories, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(max_length=500, blank=True, null=True, upload_to="media/equipments")
+
+    class Meta:
+        verbose_name = "Equipments"
+        verbose_name_plural = "Equipments"
--- website/website/settings.py
+++ website/website/settings.py
@@ -86,7 +86,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ps',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
