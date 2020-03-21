7a0c0b3f00d3a954c1163d05d3a97ea47f4c7118
SnigdhaShantharam <57173568+SnigdhaShantharam@users.noreply.github.com>
2020-03-22 02:12:00 +0530

    [snigdha]Refactor. updated models

--- website/equipments/models.py
+++ website/equipments/models.py
@@ -11,24 +11,27 @@ ratings = (
 )
 class Camera(models.Model):
     company     = models.CharField(max_length=50)
-    model       = models.CharField(max_length=50)
+    model_name       = models.CharField(max_length=50)
     description = models.TextField()
     inventory   = models.IntegerField()
     ratings     = models.IntegerField(choices=ratings)
 
     def __str__(self):
+        return '{} {}'.format(self.company, self.model_name)
+
+    class Meta:
         verbose_name = "Camera"
         verbose_name_plural = "Cameras"
 
 class Lens(models.Model):
     company     = models.CharField(max_length=50)
-    model       = models.CharField(max_length=50)
+    model_name       = models.CharField(max_length=50)
     description = models.TextField()
     inventory   = models.IntegerField()
     ratings     = models.IntegerField(choices=ratings)
     
     def __str__(self):
-        return '{} {}'.format(self.company, self.model)
+        return '{} {}'.format(self.company, self.model_name)
 
     class Meta:
         verbose_name = "Lens"
@@ -36,7 +39,7 @@ class Lens(models.Model):
 
 class Accessories(models.Model):
     company     = models.CharField(max_length=50)
-    model       = models.CharField(max_length=50)
+    model_name       = models.CharField(max_length=50)
     name        = models.CharField(max_length=150)
     description = models.TextField()
     inventory   = models.IntegerField()
@@ -51,7 +54,7 @@ class Accessories(models.Model):
 
 class Equipments(models.Model):
     camera_key = models.ForeignKey(Camera, on_delete=models.CASCADE, null=True, blank=True)
-    lens_key = models.ForeignKey(Lens, on_delete=models.CASCADE, null=True, blank=True)
+    lens_key = models.FomodelreignKey(Lens, on_delete=models.CASCADE, null=True, blank=True)
     accessories_key = models.ForeignKey(Accessories, on_delete=models.CASCADE, null=True, blank=True)
     image = models.ImageField(max_length=500, blank=True, null=True, upload_to="media/equipments")
 
