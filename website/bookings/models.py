31b309dd81ca84a4e7aa23aae77583718fc5ada6
SnigdhaShantharam <snigdhashantharam@gmail.com>
2020-05-03 02:14:10 +0530

    [snigdha]refactor. booking inventory done

--- website/bookings/models.py
+++ website/bookings/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.db.models import Q
 from django.core.exceptions import ValidationError
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator 
 # from django.contrib.auth.models import User
 
 from website.settings import AUTH_USER_MODEL
@@ -21,7 +23,7 @@ class Event(models.Model):
     start_time = models.TimeField()
     end_day = models.DateField()
     end_time = models.TimeField()
-    inventory = models.IntegerField(blank=True, null=True)
+    inventory = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0)])
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
     advance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
@@ -32,54 +34,81 @@ class Event(models.Model):
     def __str__(self):
         return '{} - {}'.format(self.customer.first_name, self.equipment_key)
 
-    def save(self, *args, **kwargs):
-        # if inventory is None:
-        #     self.inventory = 0
-        # self.inventory = inventory
-        if self.inventory is None:
-            self.inventory = (self.equipment_key.inventory - 1)
-        # else:
-        #     self.inventory = inventory
-        super(Event, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     # if inventory is None:
+    #     #     self.inventory = 0
+    #     # self.inventory = inventory
+    #     if self.inventory is None:
+    #         self.inventory = (self.equipment_key.inventory - 1)
+    #     # else:
+    #     #     self.inventory = inventory
+    #     super(Event, self).save(*args, **kwargs)
 
     def check_availability(self, fixed_startday, fixed_starttime, fixed_endday, fixed_endtime, new_startday, new_starttime, new_endday, new_endtime, inventory):
         # overlap = False
         availability = True
         if new_startday == fixed_endday or new_endday == fixed_startday:  # edge case
-
+            print("1")
             new_starttime = new_starttime.hour*60 + new_starttime.minute
             new_endtime = new_endtime.hour*60 + new_endtime.minute
             fixed_starttime = fixed_starttime.hour*60 + fixed_starttime.minute
             fixed_endtime = fixed_endtime.hour*60 + fixed_endtime.minute
 
             if (new_starttime - fixed_endtime)/60 >= 3.0:
-                availability = True
-                inventory -= 1
-                # setattr(self.inventory, value=self.inventory-1)
+                print("1--1")
+                if inventory == 0:
+                    print("1--1-->1")
+                    availability = False
+                elif inventory is None:
+                    print("1--1-->2")
+                    availability = True
+                elif inventory > 0:
+                    availability = True
+                    print("1--1-->3")
+                    inventory -= 1
             elif (fixed_starttime - new_endtime)/60 >= 3.0:
-                availability = True
-                inventory -= 1
-                # setattr(self.inventory, value=self.inventory-1)
+                print("1--2")
+                if inventory == 0:
+                    print("1--2-->1")
+                    availability = False
+                elif inventory is None:
+                    print("1--2-->2")
+                    availability = True
+                elif inventory > 0:
+                    availability = True
+                    print("1--2-->3")
+                    inventory -= 1
             else:
                 availability = False
+                print("1--3")
         elif (new_startday >= fixed_startday and new_startday <= fixed_endday) or (new_endday >= fixed_startday and new_endday <= fixed_endday):  # innner limits
             # overlap = True
+            print("2")
             if inventory == 0:
+                print("2--1")
                 availability = False
             elif inventory is None:
+                print("2--2")
                 availability = True
             elif inventory > 0:
                 availability = True
+                print("2--3")
                 inventory -= 1
         elif new_startday <= fixed_startday and new_endday >= fixed_endday:  # outter limits
             # overlap = True
+            print("3")
             if inventory == 0:
                 availability = False
+                print("3--1")
+            elif inventory is None:
+                availability = True
+                print("3--2")
             elif inventory > 0:
                 availability = True
                 inventory -= 1
+                print("3--3")
 
-        return availability, inventory
+        return availability
 
     def get_absolute_url(self):
         content_type = ContentType.objects.get_for_model(self.__class__)
@@ -89,27 +118,24 @@ class Event(models.Model):
         if self.end_day < self.start_day:
             raise ValidationError('Please check the starting and ending dates')
 
-        event = Event.objects.filter(
-            start_day=self.start_day).order_by('-id')[0]
-        if event:
-            availability, inventory = self.check_availability(
-                event.start_day, event.start_time, event.end_day, event.end_time, self.start_day, self.start_time, self.end_day, self.end_time, event.inventory)
-            if availability is True:
-                self.save(inventory)
-            else:
-                raise ValidationError(
-                    'There is an overlap with another event: ' + str(event.start_day) + ', ' + str(
-                        event.start_time) + '-' + str(event.end_time))
-        # events = Event.objects.filter(start_day=self.start_day).order_by('-id')[0]
-        # if events:
-        #     for event in events:
-        #         availability, inventory = self.check_availability(event.start_day, event.start_time, event.end_day, event.end_time, self.start_day, self.start_time, self.end_day, self.end_time, event.inventory)
-        #         if availability is True:
-        #             self.save(inventory=inventory)
-        #         else:
-        #             raise ValidationError(
-        #                 'There is an overlap with another event: ' + str(event.start_day) + ', ' + str(
-        #                     event.start_time) + '-' + str(event.end_time))
-        else:
-            self.save((self.equipment_key.inventory-1))
-            
+        # event = Event.objects.filter(start_day=self.start_day).order_by('-id')[0]
+        # events = Event.objects.filter(start_day=self.start_day).order_by('-id')
+        events = Event.objects.filter(
+            Q(start_day__exact=self.start_day, equipment_key=self.equipment_key)|
+            Q(start_day__exact=self.end_day, equipment_key=self.equipment_key)
+        ).order_by('-id')
+        # print(events)
+        if events:
+            for event in events:
+                availability= self.check_availability(event.start_day, event.start_time, event.end_day, event.end_time, self.start_day, self.start_time, self.end_day, self.end_time, event.inventory)
+                print(availability)
+                if not availability:
+                    raise ValidationError(
+                        'There is an overlap with another event: ' + str(event.start_day) + ', ' + str(
+                            event.start_time) + '-' + str(event.end_time))
+                # if availability is True:
+                #     self.save()
+                # else:
+                #     raise ValidationError(
+                #         'There is an overlap with another event: ' + str(event.start_day) + ', ' + str(
+                #             event.start_time) + '-' + str(event.end_time))
--- website/equipments/signals.py
+++
@@ -1,6 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from equipments.models import Equipment
-from bookings.models import Event
-
